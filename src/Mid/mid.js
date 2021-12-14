27b69a7ed907ae06d7dfda59147efd2bfc96f9e1
Subash Rimal <subashrimal01@gmail.com>
2021-12-14 20:59:34 +0545

    Login form created

---
+++ src/Mid/mid.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Login from "../Registration/Login";
+
+class Mid extends Component{
+    render(){
+        return(
+            <Login></Login>
+        )
+    }
+}
+
+export default Mid;
