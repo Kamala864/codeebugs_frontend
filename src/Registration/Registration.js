d63a3c76b442237479be8490bfbd8272bb0b5a52
Roshan Tamang <tgroshan42@gmail.com>
2021-12-15 19:50:58 +0545

    registration form working

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -4,29 +4,43 @@ import { Component } from "react";
 class Registration extends Component{
      render(){
      return(
-          <>
-          <div className="registration-form">
-               <div>
-                    <legend>Registration</legend>
-               </div>
-
-               <div> 
-                    <form>
-                         <div>
-                              <input type="text" className="registration-input" id="fullname" name="fullname" placeholder="Full name"/>
-                              <input type="text" className="registration-input" id="email" name="email" placeholder="Email"/>
-                              <input type="text" className="registration-input" id="age" name="age" placeholder="Age"/>
-                              <input type="password" className="registration-input" id="password" name="password" placeholder="Password"/>
-                              <input type="password" className="registration-input" id="confirm_password" name="confirm_password" placeholder="Confirm Password"/>
+          <div>
+               <div class="container" id="container">
+                    <div class="form-container sign-up-container">
+                         <form>
+                              <h1>Create Account</h1>
+                              <input type="text" placeholder="Full Name" />
+                              <input type="email" placeholder="Email" />
+                              <input type="password" placeholder="Password" />
+                              <input type="password" placeholder="Confirm Password" />
+                              <button>Sign Up</button>
+                         </form>
+                    </div>
+                    <div class="form-container sign-in-container">
+                         <form>
+                              <h1>Sign in</h1>
+                              <input type="email" placeholder="Email" />
+                              <input type="password" placeholder="Password" />
+                              <a href="#">Forgot your password?</a>
+                              <button>Sign In</button>
+                         </form>
+                    </div>
+                    <div class="overlay-container">
+                         <div class="overlay">
+                              <div class="overlay-panel overlay-left">
+                                   <h1>Welcome Back!</h1>
+                                   <p>To keep connected with us please login with your personal info</p>
+                                   <button class="ghost" id="signIn">Sign In</button>
+                              </div>
+                              <div class="overlay-panel overlay-right">
+                                   <h1>Hello, Friend!</h1>
+                                   <p>Enter your personal details and start journey with us</p>
+                                   <button class="ghost" id="signUp">Sign Up</button>
+                              </div>
                          </div>
-
-                         <div>
-                              <button type="submit">Register</button>
-                         </div>
-                    </form>
+                    </div>
                </div>
           </div>
-          </>
      )
      }
 }
