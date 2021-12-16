e97e92d65c3ecda38d9a4d6a93d15ad29ec91aa3
Roshan Tamang <tgroshan42@gmail.com>
2021-12-16 20:00:18 +0545

    registration working

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -1,7 +1,47 @@
 
 import { Component } from "react";
+import axios from "axios";
 
 class Registration extends Component{
+     
+     state = {
+          full_name : "",
+          email : "",
+          age : "",
+          password : "",
+          confirm_password : ""
+     }
+
+     changeState = (e) => {
+          this.setState({
+               [e.target.name] : e.target.value
+          })
+     }
+
+     register = (e) => {
+          e.preventDefault()
+          if (this.state.full_name === "" ||
+               this.state.email === "" ||
+               this.state.age === "" || this.state.password === "") {
+               alert("Please fill all of the informations!")
+          }
+          else {
+                if (this.state.password === this.state.confirm_password) {
+                    const data = {
+                         full_name: this.state.full_name,
+                         email: this.state.email,
+                         age: this.state.age,
+                         password: this.state.password
+                    }
+
+                    axios.post("http://localhost:5000/signup",data)
+                    alert("Registration successful!")
+               } else {
+                    alert("Password does not match!")
+               }
+          }
+     }
+
      render(){
      return(
           <div>
@@ -9,11 +49,17 @@ class Registration extends Component{
                     <div class="form-container sign-up-container">
                          <form>
                               <h1>Create Account</h1>
-                              <input type="text" placeholder="Full Name" />
-                              <input type="email" placeholder="Email" />
-                              <input type="password" placeholder="Password" />
-                              <input type="password" placeholder="Confirm Password" />
-                              <button>Sign Up</button>
+                              <input type="text" name="full_name"  placeholder="Full Name" 
+                                    value={this.state.full_name} onChange={this.changeState}/>
+                              <input type="text" name="email" placeholder="Email" 
+                                    value={this.state.email} onChange={this.changeState}/>
+                              <input type="text" name="age"  placeholder="Age" 
+                                    value={this.state.age} onChange={this.changeState}/> 
+                              <input type="password" name="password" placeholder="Password"
+                                   alue={this.state.password} onChange={this.changeState} />
+                              <input type="password" name="confirm_password" placeholder="Confirm Password"
+                                    value={this.state.confirm_password} onChange={this.changeState} />
+                              <button onClick={this.register}>Sign Up</button>
                          </form>
                     </div>
                     <div class="form-container sign-in-container">
@@ -21,7 +67,7 @@ class Registration extends Component{
                               <h1>Sign in</h1>
                               <input type="email" placeholder="Email" />
                               <input type="password" placeholder="Password" />
-                              <a href="#">Forgot your password?</a>
+                              <a>Forgot your password?</a>
                               <button>Sign In</button>
                          </form>
                     </div>
