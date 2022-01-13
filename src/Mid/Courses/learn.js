352e16899514b4fb2577c7c1020b501e0827124f
niteshRdangal <56727818+niteshRdangal@users.noreply.github.com>
2022-01-13 15:18:04 +0545

    Learning page statucally done

--- src/Mid/Courses/learn.js
+++ src/Mid/Courses/learn.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactPlayer from "react-player";
 
+
 function Learn() {
 
     return(
         <div className="Top">
-            <ReactPlayer controls url = "https://www.youtube.com/watch?v=ZmKE-iU2Qqc&t=348s" />
+            <div className ="Practice">
+            <button >Practice Now</button>
+            </div>
+            <h2>Couse title will be here</h2>
+
+            <div className="Player">
+            <ReactPlayer  controls url = "https://www.youtube.com/watch?v=ZmKE-iU2Qqc&t=348s" />
+            </div>
 
             </div>
+
     )
 }
 
