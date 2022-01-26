3dde05c3f5421887b2545f20e8d20abd3f19fa28
niteshRdangal <56727818+niteshRdangal@users.noreply.github.com>
2022-01-26 12:05:59 +0545

    Adding course completed

--- src/Mid/Courses/courseinsert.js
+++ src/Mid/Courses/courseinsert.js
@@ -1,72 +1,134 @@
-import axios from "axios";
+import { Formik, FieldArray } from "formik";
+import {TextField, Input } from "@material-ui/core";
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+import { Button } from "@material-ui/core";
+import SendIcon from '@mui/icons-material/Send';
 import { Component } from "react";
+import axios from "axios";
 
-class CourseInsert extends Component{
+class CourseInsert extends Component {
 
-    state = {
-        courseTitle : '',
-        courseDescription: '',
-        courseLecturer: '',
-        filename: null
-    }
+    render() {return <div>
+    <Formik
+    initialValues={{courseTitle : "",
+                    courseDescription : "",
+                    tutorName: "",
+                    tutorial: [{chapterName : "", video : ""}]
+                    }}
 
-    textChangeHandler=(e)=>{
-        this.setState({
-            [e.target.name] : e.target.value
+    
+    onSubmit={values =>{
+        console.log('Submit: ',values);
+
+        axios.post("http://localhost:5000/addcourse",{
+          courseTitle : values.courseTitle,
+          courseDescription : values.courseDescription,
+          tutorName : values.tutorName,
+          tutorial : values.tutorial
         })
-    }
 
-    fileChangeHandler= (e)=>{
-        this.setState({
-            filename: e.target.files[0]
-        })
-    }
-
-
-    sendData = (e)=>{
-        e.preventDefault();
-        const data =  new FormData();
-
-        data.append('title', this.state.courseTitle)
-        data.append('description', this.state.courseDescription)
-        data.append('lecturer', this.state.courseLecturer)
-        data.append('video', this.state.filename)
-        axios.post("http://localhost:5000/addcourse", data)
         .then((result)=>{
+
             console.log(result)
         })
+
         .catch()
-        // window.location.href="/courseinsert"
-    }
+    }}
 
-    render(){
-        return(
-                <section className="class-area2 bg-fdf6ed pt-100 pb-70">
-                <div className="cont" id="container">
-                    <div className="d-flex justify-content-center">
-                <div className="form-container col-lg-8 ">
-                            <form>
-                            <h1>Add Course</h1>
-                                <div class="row">
-                                <input type="text" name="courseTitle" placeholder="Course Title"
-                                value={this.state.courseTitle} onChange={this.textChangeHandler}/>                               
-                                <input type="text" name="courseDescription" placeholder="Course Description"
-                                value={this.state.courseDescription} onChange={this.textChangeHandler}/>
-                                <input type="text" name="courseLecturer" placeholder="Course Lecturer"
-                                value={this.state.courseLecturer} onChange={this.textChangeHandler}/>
-                                <label>Upload Video Course</label>
-                                <input type="file" accept="video/mp4" name="filename" onChange={this.fileChangeHandler}/>                              
-                                <div className="center"><button type="submit" onClick={this.sendData}>Add</button></div>
-                                </div>
-                            </form>
-                        </div>
-                        </div>
-                        </div>
-                </section>
-        )
-    }
+    
 
+    > 
+    {({values  , handleChange, handleBlur, handleSubmit}) =>(
+        <form onSubmit={handleSubmit}>
+        <TextField 
+        style={{width : 480}}
+        variant="standard"
+        label= "Course Title"
+        name="courseTitle" 
+        values={values.courseTitle} 
+        onChange={handleChange} 
+        onBlur={handleBlur}/>
 
+        <TextField 
+        style={{width : 480}}
+        variant="standard"
+        label= "Tutor Name"
+        name="tutorName" 
+        values={values.tutorName} 
+        onChange={handleChange} 
+        onBlur={handleBlur}/>
+
+        <TextField 
+        style={{width : 480}}
+        multiline
+        variant="standard"
+        label= "Course Description"
+        name="courseDescription" 
+        values={values.courseDescription} 
+        onChange={handleChange} 
+        onBlur={handleBlur}/>
+
+    <FieldArray name="tutorial">
+    {arrayHelpers => (
+        <div>
+          <Button
+            onClick={() =>
+              arrayHelpers.push({
+                chapterName : "",
+                video: "",
+              })
+            }
+          >
+            add Chapters
+          </Button>
+          {values.tutorial.map((chapterName, index) => {
+            return (
+              <div >
+              <Stack direction="row" alignItems="center" spacing={3}>
+              <TextField 
+              style={{width : 280}}
+              multiline
+              variant="standard"
+              label= "chapter Name"
+              name={`tutorial.${index}.chapterName`} 
+              values={`tutorial.${index}.chapterName`} 
+              onChange={handleChange} 
+              onBlur={handleBlur}/>
+
+              <label htmlFor="icon-button-file">
+              <Input name={`tutorial.${index}.video`}
+              values={`tutorial.${index}.video`} 
+              accept="video/mp4" type="file"
+              onChange={handleChange}
+                />
+              <IconButton color="primary" aria-label="upload video" component="span">
+                <PhotoCamera />
+              </IconButton>
+            </label>
+                
+                <Button onClick={() => arrayHelpers.remove(index)}>
+                  x
+                </Button>
+                </Stack>
+              </div>
+            );
+          })}
+        </div>
+      )}
+        
+    </FieldArray>
+
+        <Button endIcon={<SendIcon/>} varient="contained" type="submit">Submit</Button>
+        <pre>{JSON.stringify(values)}</pre>
+        </form>
+    )}
+    
+    </Formik>
+
+    </div>;
+}
 }
 
 export default CourseInsert;
