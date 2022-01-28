479d3a110e048e27a034314e7ec8edac532fe317
niteshRdangal <56727818+niteshRdangal@users.noreply.github.com>
2022-01-28 19:08:19 +0545

    Quiz post and fetch work completed

--- src/Mid/Courses/learn.js
+++ src/Mid/Courses/learn.js
@@ -1,6 +1,13 @@
 import React, { Component, useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -10,19 +17,23 @@ class Learn extends Component {
         tutorial : [{chapterName:"dummy"}],
         tutorName : "Test",
         courseDescription : "",
-        video : ""
+        video : "",
+        quiz:[],
+        divcontainer : false,
+        index : 0,
     }
 
 
 
     componentDidMount(){
-        axios.get("http://localhost:5000/course/61f0d527ec21a85d2d89cf4c")
+        axios.get("http://localhost:5000/course/61f3e3c34f09a9686ff3429e")
         .then(res => {
             this.setState({
                 courseTitle : (res.data.courseTitle),
                 courseDescription : (res.data.courseDescription),
                 tutorial:(res.data.tutorial),
-                tutorName: (res.data.tutorName)
+                tutorName: (res.data.tutorName),
+                quiz  : (res.data.quiz)
             })
             
             // console.log(res.data)
@@ -35,10 +46,35 @@ class Learn extends Component {
     
 
     render(){
+
+
+
+        var quizfunc = e =>
+        {
+            e.preventDefault()
+            this.setState({divcontainer: !this.state.divcontainer});
+            var element = document.getElementById("player");
+            element.remove();
+        }
+
+
+        const quiz=this.state.divcontainer
+
+        const handleQuestionOnclick = () => {
+            this.setState(index => {
+                if (index === quiz.length - 1 ){
+                    return 0;
+                }else{
+                    return index + 1;
+                }
+            })
+        }
+
     return(
         <div className="Top">
             <div className ="Practice">
             <button >Practice Now</button>
+            
             </div>
             <h2>{this.state.courseTitle}</h2>
 
@@ -52,6 +88,9 @@ class Learn extends Component {
                         }}>{cObj.chapterName}</a>
                         )
                     )}
+
+                    <a href="" onClick={quizfunc}>{quiz?'Quiz': 'quiz'}</a>
+                    
                     
 
 
@@ -59,16 +98,50 @@ class Learn extends Component {
 
                 <div className="column right">
                     
-                    
-                <ReactPlayer width={1200}
+                    <ReactPlayer width={1200} id='player'
                 url= {"http//localhost:5000/" + this.state.video }
                 controls
                 />
+                
+
+                {
+                    quiz &&(<div className="quizCard" >
+                    
+                            <Card sx={{ maxWidth: 1200 }}>
+                                <CardContent>
+                                {
+                                    this.state.quiz.map((qObj)=>
+                                    <div>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                    
+                                        {qObj.question}
+                                        
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        <FormControlLabel control={<Checkbox  />} label={qObj.correctAnswer} />
+                                        
+                                            <p><FormControlLabel control={<Checkbox  />} label={qObj.incorrectAnswer[0]} /></p>
+                                            <p><FormControlLabel control={<Checkbox  />} label={qObj.incorrectAnswer[1]} /></p>
+                                            <p><FormControlLabel control={<Checkbox  />} label={qObj.incorrectAnswer[2]} /></p>
+                                    </Typography>
+                                    </div>
+                                )
+                                }
+                                </CardContent>
+                                <CardActions>
+                                    <Button variant="contained" onClick={handleQuestionOnclick}>Next</Button>
+                                </CardActions>
+                            </Card>
+
+                        
+                        
+                        </div>)
+                }
 
                 
                 </div>
             </div>
-            <hr></hr>
+            <hr className="line_break"></hr>
 
             <div>
             <h3 className="description">Description</h3>
