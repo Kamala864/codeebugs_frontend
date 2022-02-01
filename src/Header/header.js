28aaa364894b6745909e306e495ad228bb669b5a
David Joshi <david.joshi013@gmail.com>
2022-02-01 19:33:34 +0545

    notification section added

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,32 +1,74 @@
 import { Component } from "react";
 
-class Header extends Component{
-  logOut=(e)=>{
+class Header extends Component {
+  logOut = (e) => {
     localStorage.clear()
-    window.location.href="/login"
+    window.location.href = "/login"
   }
-    render() {
+  render() {
 
-      if (localStorage.getItem('token')){
-        var menu = <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+    if (localStorage.getItem('token')) {
+      var menu = <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
             <a href="/" className="nav-link active">
               Home
             </a>
-         
+
           </li>
           <li className="nav-item">
             <a href="/courses" className="nav-link">
               Courses
-              
+
             </a>
-            
+
           </li>
           <li className="nav-item">
             <a href="#" className="nav-link">
-              Payments
+              About Us
             </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Notifications
+            </a>
+          </li>
+        </ul>
+
+
+
+        <div className="others-options d-flex align-items-center mr-3">
+          <div class="input-group">
+            <div class="form-outline">
+              <input id="search-input" type="search" id="form1" class="form-control rounded" placeholder="Search" />
+            </div>
+          </div>
+        </div>
+
+        <div className="others-options d-flex align-items-center">
+          <div class="nav-item">
+            <a href="/userprofile" className="nav-link font-weight-bold">
+              {localStorage.getItem('username')}
+            </a>
+          </div>
+        </div>
+
+      </div>
+    }
+    else {
+      var menu = <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="/" className="nav-link active">
+              Home
+            </a>
+
+          </li>
+          <li className="nav-item">
+            <a href="/courses" className="nav-link">
+              Courses
+            </a>
+
           </li>
           <li className="nav-item">
             <a href="contact.html" className="nav-link">
@@ -35,55 +77,16 @@ class Header extends Component{
           </li>
         </ul>
 
+
         <div className="others-options d-flex align-items-center mr-3">
           <div class="input-group">
             <div class="form-outline">
               <input id="search-input" type="search" id="form1" class="form-control rounded" placeholder="Search" />
             </div>
           </div>
-          </div>
-        
-        <div className="others-options d-flex align-items-center">
-        <div class="nav-item">
-        <a href="/userprofile" className="nav-link font-weight-bold">
-              {localStorage.getItem('username')}
-            </a>
         </div>
-        </div>
-        </div>
-      }
-      else{
-        var menu = <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a href="/" className="nav-link active">
-              Home
-            </a>
-         
-          </li>
-          <li className="nav-item">
-            <a href="/courses" className="nav-link">
-              Courses          
-            </a>
-
-          </li>
-          <li className="nav-item">
-            <a href="contact.html" className="nav-link">
-              About Us
-            </a>
-          </li>
-          </ul>
 
 
-          <div className="others-options d-flex align-items-center mr-3">
-          <div class="input-group">
-            <div class="form-outline">
-              <input id="search-input" type="search" id="form1" class="form-control rounded" placeholder="Search" />
-            </div>
-          </div>
-          </div>
-
-  
 
         <div className="others-options d-flex align-items-center">
           <div className="option-item">
@@ -91,45 +94,45 @@ class Header extends Component{
           </div>
         </div>
       </div>
-      }
-
-        return(
-        <div className="navbar-area">
-  <div className="main-responsive-nav">
-    <div className="container">
-      <div className="main-responsive-menu">
-        <div className="logo">
-          <a href="/">
-            <img src="logo.png" alt="image" />
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="main-navbar">
-    <div className="container">
-      <nav className="navbar navbar-expand-md navbar-light">
-        <a className="navbar-brand" href="/">
-          <img src="logo.png" alt="image" />
-        </a>
-          {menu}
-      </nav>
-    </div>
-  </div>
-  <div className="others-option-for-responsive">
-    <div className="container">
-      <div className="dot-menu">
-        <div className="inner">
-          <div className="circle circle-one" />
-          <div className="circle circle-two" />
-          <div className="circle circle-three" />
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-)
     }
+
+    return (
+      <div className="navbar-area">
+        <div className="main-responsive-nav">
+          <div className="container">
+            <div className="main-responsive-menu">
+              <div className="logo">
+                <a href="/">
+                  <img src="logo.png" alt="image" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-navbar">
+          <div className="container">
+            <nav className="navbar navbar-expand-md navbar-light">
+              <a className="navbar-brand" href="/">
+                <img src="logo.png" alt="image" />
+              </a>
+              {menu}
+            </nav>
+          </div>
+        </div>
+        <div className="others-option-for-responsive">
+          <div className="container">
+            <div className="dot-menu">
+              <div className="inner">
+                <div className="circle circle-one" />
+                <div className="circle circle-two" />
+                <div className="circle circle-three" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header;
