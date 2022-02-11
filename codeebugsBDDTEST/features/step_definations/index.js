18390fad77c289fe09db698b535c2c979b619a98
niteshRdangal <56727818+niteshRdangal@users.noreply.github.com>
2022-02-11 12:42:06 +0545

    Registration test functionality success

--- codeebugsBDDTEST/features/step_definations/index.js
+++ codeebugsBDDTEST/features/step_definations/index.js
@@ -2,9 +2,11 @@ const {expect} = require("chai");
 const {Given, When, Then, Before, After} = require("@cucumber/cucumber");
 const { Builder, By, Key, until, sleep } = require("selenium-webdriver");
 const { delay } = require("../utils/delay");
+require('chromedriver')
 
 Given('Test registration functionality', {timeout: 30000}, async function (){
-    let driver = await new Builder().forBrowser("chrome").build();
+    var webdriver = require('selenium-webdriver') ; 
+    var driver = new webdriver.Builder().forBrowser('chrome').build();
     await driver.get("http://localhost:3000/login");
     await driver.findElement(By.id("signUp")).click();
     await driver.findElement(By.id("full_name")).sendKeys("testBDD");
@@ -13,8 +15,9 @@ Given('Test registration functionality', {timeout: 30000}, async function (){
     await driver.findElement(By.id("password")).sendKeys("12345678");
     await driver.findElement(By.id("confirm_password")).sendKeys("12345678");
     await driver.sleep(delay);
+    await driver.findElement(By.id("btnSignup")).click()
     await driver.wait(until.elementLocated(By.id("loginemail")), 30000);
-    expect(await driver.wait(until.elementLocated(by.id("loginemail"))));
+    expect(await driver.wait(until.elementLocated(By.id("loginemail"))));
     await driver.quit()
 
 })
