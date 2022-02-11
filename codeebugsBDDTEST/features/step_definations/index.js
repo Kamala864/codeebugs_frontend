8ba7ef278ae0d8beb37cb9628d6833beeb860b8d
niteshRdangal <56727818+niteshRdangal@users.noreply.github.com>
2022-02-11 14:11:12 +0545

    Login test Completed

--- codeebugsBDDTEST/features/step_definations/index.js
+++ codeebugsBDDTEST/features/step_definations/index.js
@@ -2,9 +2,14 @@ const {expect} = require("chai");
 const {Given, When, Then, Before, After} = require("@cucumber/cucumber");
 const { Builder, By, Key, until, sleep } = require("selenium-webdriver");
 const { delay } = require("../utils/delay");
+require('chromedriver');
 
 Given('Test registration functionality', {timeout: 30000}, async function (){
-    let driver = await new Builder().forBrowser("chrome").build();
+
+    var webdriver = require('selenium-webdriver');
+    var driver = new webdriver.Builder().forBrowser('chrome').build();
+
+
     await driver.get("http://localhost:3000/login");
     await driver.findElement(By.id("signUp")).click();
     await driver.findElement(By.id("full_name")).sendKeys("testBDD");
@@ -16,6 +21,21 @@ Given('Test registration functionality', {timeout: 30000}, async function (){
     await driver.findElement(By.id("btnSignup")).click()
     await driver.wait(until.elementLocated(By.id("loginemail")), 30000);
     expect(await driver.wait(until.elementLocated(By.id("loginemail"))));
+    //await driver.quit()
+})
+
+Given ('Test login functionality', {timeout:40000}, async function(){
+    var webdriver = require('selenium-webdriver');
+    var driver = new webdriver.Builder().forBrowser('chrome').build();
+
+    await driver.get("http://localhost:3000/login");
+
+    await driver.findElement(By.id("loginemail")).sendKeys("testBDD@gmail.com");
+    await driver.findElement(By.id("loginpassword")).sendKeys("12345678");
+    await driver.findElement(By.id("login_signin")).click()
+    
+    await driver.wait(until.elementLocated(By.id("logo")), 30000);
+    expect(await driver.wait(until.elementLocated(By.id("logo"))));
     await driver.quit()
 
 })
